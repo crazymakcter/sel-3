161339a0521b36ec82d4a8d5393bc8a55a61a3dc
crazymakcter <crazymakcter@gmail.com>
2017-01-29 00:21:20 +0200

    First commet
    Default app

---
+++ Sel-3-01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Sel_3_01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (IWebDriver driver = new ChromeDriver(@"C:\web_config"))
+            {
+                //Notice navigation is slightly different than the Java version
+                //This is because 'get' is a keyword in C#
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+                // Find the text input element by its name
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+                // Enter something to search for
+                query.SendKeys("Cheese");
+
+                // Now submit the form. WebDriver will find the form for us from the element
+                query.Submit();
+
+                // Google's search is rendered dynamically with JavaScript.
+                // Wait for the page to load, timeout after 10 seconds
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+                // Should see: "Cheese - Google Search" (for an English locale)
+                Console.WriteLine("Page title is: " + driver.Title);
+                Console.ReadKey();
+            }
+        }
+    }
+}
